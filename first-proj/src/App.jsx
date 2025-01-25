66db93f83efedf2bcafeed4e2544587558fc15df
JlordS32 <jlordsty128@gmail.com>
2025-01-26 09:58:40 +1100

    added tailwind

--- first-proj/src/App.jsx
+++ first-proj/src/App.jsx
@@ -1,31 +1,9 @@
-const Card = ({ title }) => {
-   const [hasLiked, setHasLiked] = useState(false);
-   
-   return (
-      <div className="card">
-         <h1>{title}</h1>
-         <button onClick={() => setHasLiked(true)}>Like</button>
-      </div>
-   );
-};
-
-// Function to generate the cards
-const generateCards = (count) => {
-   const cards = [];
-   for (let i = 0; i < count; i++) {
-      cards.push(<Card key={i} title={`Card ${i + 1}`} />);
-   }
-   return cards;
-};
+import React from "react";
 
 const App = () => {
    
-
-   return <div className="card-container">
-      <Card title="Iron Man" />
-      <Card title="Penguin Shits" />
-      <Card title="Moana" />
-   </div>;
+   
+   return <div>App</div>;
 };
 
 export default App;
