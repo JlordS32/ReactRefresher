cb18fbc4be36a5d231f68d59687fe314e1007bec
JlordS32 <jlordsty128@gmail.com>
2025-01-27 06:56:18 +1100

    moved it back

--- first-proj/src/App.jsx
+++ first-proj/src/App.jsx
@@ -1,13 +1,51 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import MovieCard from "./components/MovieCard";
-import fetchMovies from "./helpers/fetchMovies";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
+const API_OPTIONS = {
+   method: "GET",
+   headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+   },
+};
 
 const App = () => {
    const [searchTerm, setSearchTerm] = useState("");
    const [errorMessage, setErrorMessage] = useState("");
    const [movieList, setMovieList] = useState({});
    const [isLoading, setIsLoading] = useState(true);
+
+   const fetchMovies = async (setIsLoading, setErrorMessage, setMovieList) => {
+      setIsLoading(true);
+      setErrorMessage("");
+
+      try {
+         const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+         const response = await fetch(endpoint, API_OPTIONS);
+
+         if (!response.ok) {
+            throw new Error("Failed to fetch movies!");
+         }
+
+         const data = await response.json();
+
+         if (data.Response === "False") {
+            setErrorMessage(data.Error || "Failed to fetch movies!");
+            setMovieList([]);
+            return;
+         }
+
+         setMovieList(data.results || []);
+      } catch (err) {
+         console.error(`Error fetching movies: ${err}`);
+         setErrorMessage(err.message);
+      } finally {
+         setIsLoading(false);
+      }
+   };
 
    useEffect(() => {
       console.log(isLoading);
