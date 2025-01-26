dc1fdb87e840a645dc703330fc703f7bd463c6bc
JlordS32 <jlordsty128@gmail.com>
2025-01-26 12:01:12 +1100

    update

--- first-proj/src/App.jsx
+++ first-proj/src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 const App = () => {
-   
-   
-   return <div>App</div>;
+   return (
+      <main>
+         <div className="">
+            <div className="wrapper">
+               <header>
+                  <h1>
+                     Find <span className="text-gradient">Movies</span> You'll
+                     Enjoy Without the Hassle!
+                  </h1>
+               </header>
+            </div>
+         </div>
+      </main>
+   );
 };
 
 export default App;
