2c47e17ee3dbc6a42a8e1dc4106ed5313a08051f
JlordS32 <jlordsty128@gmail.com>
2025-01-27 10:00:07 +1100

    fetching

--- first-proj/src/App.jsx
+++ first-proj/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
+import { Client } from "appwrite";
+import { updateSearchCount, getTrendingMovies } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
@@ -15,15 +18,21 @@ const API_OPTIONS = {
 const App = () => {
    const [searchTerm, setSearchTerm] = useState("");
    const [errorMessage, setErrorMessage] = useState("");
-   const [movieList, setMovieList] = useState({});
+   const [movieList, setMovieList] = useState([]);
+   const [trendingMovies, setTrendingMovies] = useState([]);
    const [isLoading, setIsLoading] = useState(true);
+   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
-   const fetchMovies = async (setIsLoading, setErrorMessage, setMovieList) => {
+   useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+   const fetchMovies = async (query = "") => {
       setIsLoading(true);
       setErrorMessage("");
 
       try {
-         const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+         const endpoint = query
+            ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+            : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
          const response = await fetch(endpoint, API_OPTIONS);
 
          if (!response.ok) {
@@ -39,6 +48,11 @@ const App = () => {
          }
 
          setMovieList(data.results || []);
+
+         if (query && data.results.length > 0) {
+            console.log("Updating search count...");
+            updateSearchCount(query, data.results[0]);
+         }
       } catch (err) {
          console.error(`Error fetching movies: ${err}`);
          setErrorMessage(err.message);
@@ -47,10 +61,28 @@ const App = () => {
       }
    };
 
+   const loadTrendingMovies = async () => {
+      try {
+         const movies = await getTrendingMovies();
+         setTrendingMovies(movies);
+         console.log(movies);
+      } catch (err) {
+         console.error(`Error fetching movies: ${err}`);
+      }
+   };
+
    useEffect(() => {
-      console.log(isLoading);
-      fetchMovies(setIsLoading, setErrorMessage, setMovieList);
+      updateSearchCount();
+      fetchMovies(debouncedSearchTerm);
+   }, [debouncedSearchTerm]);
+
+   useEffect(() => {
+      loadTrendingMovies();
    }, []);
+
+   useEffect(() => {
+      console.log(trendingMovies);
+   }, [trendingMovies]);
 
    return (
       <main>
@@ -59,12 +91,30 @@ const App = () => {
                <header>
                   <img src="./hero-img.png" alt="Hero Image" />
                   <h1>
-                     Find <span className="text-gradient">Movies</span> You'll
-                     Enjoy Without the Hassle!
+                     Find <span className="text-gradient">Movies</span>{" "}
+                     You&apos;ll Enjoy Without the Hassle!
                   </h1>
+                  <Search
+                     searchTerm={searchTerm}
+                     setSearchTerm={setSearchTerm}
+                  />
                </header>
 
-               <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+               {trendingMovies && trendingMovies.length > 0 && (
+                  <section className="trending">
+                     <h2>Trending Movies</h2>
+
+                     {/* <ul>
+                        {trendingMovies.map((movie) => {
+                           return (
+                              <li key={movie.$id}>
+                                 <p>{index + 1}</p>
+                              </li>
+                           );
+                        })}
+                     </ul> */}
+                  </section>
+               )}
 
                <section className="all-movies pt-7">
                   <h2>All Movies</h2>
