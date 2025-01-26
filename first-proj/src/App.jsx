26fa06c53adaaad562a0ac96dfea084e46350e9e
JlordS32 <jlordsty128@gmail.com>
2025-01-27 06:34:19 +1100

    decapsulated api fetching logic

--- first-proj/src/App.jsx
+++ first-proj/src/App.jsx
@@ -1,15 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
-
-const API_BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
-const API_OPTIONS = {
-   method: "GET",
-   headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${API_KEY}`,
-   },
-};
+import MovieCard from "./components/MovieCard";
+import fetchMovies from "./helpers/fetchMovies";
 
 const App = () => {
    const [searchTerm, setSearchTerm] = useState("");
@@ -17,38 +9,9 @@ const App = () => {
    const [movieList, setMovieList] = useState({});
    const [isLoading, setIsLoading] = useState(true);
 
-   const fetchMovies = async () => {
-      setIsLoading(true);
-      setErrorMessage("");
-
-      try {
-         const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-         const response = await fetch(endpoint, API_OPTIONS);
-
-         if (!response.ok) {
-            throw new Error("Failed to fetch movies!");
-         }
-
-         const data = await response.json();
-
-         if (data.Response === "False") {
-            setErrorMessage(data.Error || "Failed to fetch movies!");
-            setMovieList([]);
-            return;
-         }
-
-         setMovieList(data.results || []);
-      } catch (err) {
-         console.error(`Error fetching movies: ${err}`);
-         setErrorMessage(err.message);
-      } finally {
-         setIsLoading(false);
-      }
-   };
-
    useEffect(() => {
       console.log(isLoading);
-      fetchMovies();
+      fetchMovies(setIsLoading, setErrorMessage, setMovieList);
    }, []);
 
    return (
@@ -69,7 +32,7 @@ const App = () => {
                   <h2>All Movies</h2>
 
                   {isLoading ? (
-                     <p>Loading ...</p>
+                     <p className="text-white">Loading ...</p>
                   ) : errorMessage ? (
                      <p className="text-red-500">{errorMessage}</p>
                   ) : movieList.length === 0 ? (
