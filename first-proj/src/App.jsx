66c64c7245d33e4a9c386322675feff014e40f89
JlordS32 <jlordsty128@gmail.com>
2025-02-23 12:02:29 +1100

    done

--- first-proj/src/App.jsx
+++ first-proj/src/App.jsx
@@ -104,15 +104,16 @@ const App = () => {
                   <section className="trending">
                      <h2>Trending Movies</h2>
 
-                     {/* <ul>
-                        {trendingMovies.map((movie) => {
+                     <ul>
+                        {trendingMovies.map((movie, index) => {
                            return (
                               <li key={movie.$id}>
                                  <p>{index + 1}</p>
+                                 <img src={movie.poster_url} alt={movie.title} />
                               </li>
                            );
                         })}
-                     </ul> */}
+                     </ul>
                   </section>
                )}
 
